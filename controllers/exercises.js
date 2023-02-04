2b6453b5f660de5e0e8d3dee51e0295271955747
Manny <emaldonado1402@gmail.com>
2023-02-04 14:13:12 -0500

    Stub up in Routes

--- controllers/exercises.js
+++ controllers/exercises.js
@@ -1,7 +1,23 @@
 import { Exercise } from "../models/exercise"
 import { v2 as cloudinary } from 'cloudinary'
 
-function index(req, res) {
-  Exercise.find({})
-  .then
+const create = async (req, res) => {
+  try {
+    req.body.author = req.user.profile
+    const exercise = await Exercise.create(req.body)
+    const profile = await Exercise.findByIdAndUpdate(
+      req.user.profile,
+      { $push: { exercises: exercise } },
+      { new : true }
+      )
+      exercise.author = profile
+      res.status(201).json(exercise)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
+}
+
+export {
+  create,
 }
