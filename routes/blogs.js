b844ddc231bbda7d70ad448479854d8d116d7364
JakeTodoroki <taco7897@gmail.com>
2023-02-04 12:42:07 -0700

    index controller and route

--- routes/blogs.js
+++ routes/blogs.js
@@ -9,6 +9,7 @@ const router = Router()
 
 // ========= Protected Routes ========= 
 router.use(decodeUserFromToken)
+router.get('/', checkAuth, blogsCtrl.index)
 router.post('/', checkAuth, blogsCtrl.create)
 
 export { router }
