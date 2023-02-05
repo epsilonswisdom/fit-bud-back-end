6091d9ada23ee827d7e2590bb82679a03521bd3f
Manny <emaldonado1402@gmail.com>
2023-02-05 15:34:04 -0500

    Create an index for Exercise

--- controllers/exercises.js
+++ controllers/exercises.js
@@ -1,17 +1,18 @@
 import { Exercise } from "../models/exercise.js"
-import { v2 as cloudinary } from 'cloudinary'
+import { Profile } from "../models/profile.js"
+// import { v2 as cloudinary } from 'cloudinary'
 
 const create = async (req, res) => {
   try {
     req.body.author = req.user.profile
     const exercise = await Exercise.create(req.body)
-    const profile = await Exercise.findByIdAndUpdate(
+    const profile = await Profile.findByIdAndUpdate(
       req.user.profile,
       { $push: { exercises: exercise } },
       { new : true }
-      )
-      exercise.author = profile
-      res.status(201).json(exercise)
+    )
+    exercise.author = profile
+    res.status(201).json(exercise)
   } catch (error) {
     console.log(error)
     res.status(500).json(error)
