235a1b7b56e42d8c0d8237a467cb08c34aad5d6c
Mike Robles <mikej.robles@gmail.com>
2023-02-07 12:39:56 -0500

    create meal comment

--- models/meal.js
+++ models/meal.js
@@ -2,10 +2,22 @@ import mongoose from "mongoose"
 
 const Schema = mongoose.Schema
 
+const commentSchema = new Schema(
+  {
+    text: {
+      type: String,
+      required: true
+    },
+    author: { type:Schema.Types.ObjectId, ref: 'Profile' }
+  },
+  { timestamps: true }
+)
+
 const mealSchema = new Schema ({
   name: String,
   author: { type: Schema.Types.ObjectId, ref: "Profile"},
   description: String,
+  comments: [ commentSchema ]
 }, {
   timestamps: true
 })
